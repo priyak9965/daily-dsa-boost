b1d0d806f11494bd3b95599da7e8a003bbe8ba0c
priyak9965 <57129722+priyak9965@users.noreply.github.com>
2025-06-20 14:39:23 +0530

    Create inputOutput.java

---
+++ day03/inputOutput.java
@@ -0,0 +1,18 @@
+/*Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+
+Use:-
+
+for C++ : cout << variable_name;
+for Java : System.out.print();
+for Python : print()
+for Javascript : console.log()*/
+
+
+class Solution {
+    public void printNumber(Scanner sc) {
+        int num = sc.nextInt();  
+        System.out.print(num);
+    }
+}
