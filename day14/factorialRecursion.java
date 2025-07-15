f6a53091854195512090543ae426e832b2cefceb
priyak9965 <57129722+priyak9965@users.noreply.github.com>
2025-07-15 22:11:57 +0530

    Create factorialRecursion.java

---
+++ day14/factorialRecursion.java
@@ -0,0 +1,5 @@
+/* Given a number X,  print its factorial.
+
+To obtain the factorial of a number, it has to be multiplied by all the whole numbers preceding it. More precisely X! = X*(X-1)*(X-2) … 1.
+
+Note: X  is always a positive number. */
