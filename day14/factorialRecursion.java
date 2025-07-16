ed55e30ac4a9aebecf8090a9cfc92e3d22b0b32c
priyak9965 <57129722+priyak9965@users.noreply.github.com>
2025-07-16 22:49:12 +0530

    Update factorialRecursion.java

--- day14/factorialRecursion.java
+++ day14/factorialRecursion.java
@@ -3,3 +3,23 @@
 To obtain the factorial of a number, it has to be multiplied by all the whole numbers preceding it. More precisely X! = X*(X-1)*(X-2) … 1.
 
 Note: X  is always a positive number. */
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int fact = 1;
+        int count = 0;
+        for (int i = 1; i<=n ; i++){
+            fact = fact * i;
+        
+        }
+    while(fact>0){
+        if(fact%10 == 0){
+            count ++;
+            fact= fact/10;
+        }
+        else return count;
+    } 
+    return count; 
+        
+    }
+}
