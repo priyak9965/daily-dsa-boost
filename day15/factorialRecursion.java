3195f27453ca8e227d363c50a7c610e5d84ab53b
priyak9965 <57129722+priyak9965@users.noreply.github.com>
2025-07-19 12:40:33 +0530

    Create factorialRecursion.java

---
+++ day15/factorialRecursion.java
@@ -0,0 +1,25 @@
+/*Given a positive integer, n. Find the factorial of n.
+
+Examples :
+
+Input: n = 5
+Output: 120
+Explanation: 1 x 2 x 3 x 4 x 5 = 120
+Input: n = 4
+Output: 24
+Explanation: 1 x 2 x 3 x 4 = 24
+Constraints:
+0 ≤ n ≤ 12*/
+
+class Solution {
+    static int factorial(int n) {
+        // code here
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        if(n>1)
+        return n*factorial(n-1);
+        // return (n*(n-1));
+        
+        return 1;
+    }
+}
