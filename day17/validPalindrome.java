fb45712b6ede52e17669b58a8799a4362c1e1a42
priyak9965 <57129722+priyak9965@users.noreply.github.com>
2025-07-27 19:19:44 +0530

    Update validPalindrome.java

--- day17/validPalindrome.java
+++ day17/validPalindrome.java
@@ -27,3 +27,39 @@ Constraints:
 1 <= s.length <= 2 * 105
 s consists only of printable ASCII characters.
 */
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase(); //this will put the whole string in lowecase for uniformity.
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while(start < end) {
+            char left = s.charAt(start); //you have to use charAt for this, you cannot use s[start] for strings in java like in C++, string is not treated like array of characters in java
+            char right = s.charAt(end);
+            
+          
+            if (!((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9'))) { //exclude the characters that are not alphanumeric
+                start++;
+                continue;
+            }
+            if (!((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
+                end--;
+                continue;
+            }
+
+            if (left == right) {
+                start++;
+                end--;
+                continue;
+            }
+
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
